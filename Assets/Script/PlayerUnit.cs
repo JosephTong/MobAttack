06f8b0aefc21ca8f728a1a5bb9422fbf5900bdaf
JosephTong <joseph.tong@titansworld.co>
2023-08-23 17:33:15 +0800

    unity controller

---
+++ Assets/Script/PlayerUnit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnit : UnitController
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 11)
+        {// EnemyUnit
+            if (collision.gameObject.TryGetComponent<UnitController>(out UnitController hitedUnit))
+            {
+                float damage = Mathf.Min( GetHp(), hitedUnit.GetHp() );
+                SetHp( GetHp() - damage );
+                hitedUnit.SetHp( hitedUnit.GetHp() - damage);
+            }
+        }
+    }
+}
--- Assets/Script/UnitController.cs
+++ Assets/Script/UnitController.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum UnitTeam
+{
+    Player = 0,
+    Enemy
+}
+
 public class UnitController : MonoBehaviour
 {
-    [SerializeField] private Transform m_Unit;
-    [SerializeField] private float m_Hp = 1;
-    [SerializeField] private float m_Speed = 1;
-    [SerializeField] private Rigidbody m_Rigidbody;
+    private GameObject m_Unit;
+    private float m_Hp = 1;
+    private float m_Speed = 1;
+    private Rigidbody m_Rigidbody;
+    private UnitTeam m_Team;
+    
+    
+
+    public void Init(float hp, float speed, UnitTeam team,GameObject gameObject){
+        m_Hp = hp;
+        m_Speed = speed; 
+        m_Team = team;
+        m_Unit = gameObject;
+        m_Unit.transform.localScale = Vector3.one * (Mathf.Max(1,m_Hp));
+    }
 
     // Update is called once per frame
     private void FixedUpdate() {
@@ -21,4 +38,24 @@ public class UnitController : MonoBehaviour
         //m_Rigidbody.AddForce(m_Unit.forward *m_Speed*Time.deltaTime);
 
     }
+
+    public void SetHp(float hp){
+        if(m_Hp<=0){
+            return;
+        }
+
+        m_Hp = hp;
+
+        m_Unit.transform.localScale = Vector3.one * (Mathf.Max(1,m_Hp));
+
+        if(hp<=0){
+            Destroy(m_Unit,1);
+        }
+    }
+
+    public float GetHp(){
+        return m_Hp;
+    }
+
+
 }
